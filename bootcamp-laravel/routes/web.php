88d6de3533b559e60cd371b51b5a21a39d91e0be
Fawwas Alghifari <fawwasalghifari20@gmail.com>
2026-05-19 11:44:44 +0700

    new update

--- bootcamp-laravel/routes/web.php
+++ bootcamp-laravel/routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
 
+// 1. Halaman Utama (/)
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ini adalah Halaman Utama Ecommerce';
+});
+
+// 2. Halaman Produk (/products)
+Route::get('/products', function () {
+    return 'Ini adalah Halaman Daftar Produk';
+});
+
+// 3. Halaman Keranjang Belanja (/cart)
+Route::get('/cart', function () {
+    return 'Ini adalah Halaman Keranjang Belanja Anda';
+});
+
+// 4. Halaman Checkout (/checkout)
+Route::get('/checkout', function () {
+    return 'Ini adalah Halaman Proses Checkout Pembayaran';
 });
